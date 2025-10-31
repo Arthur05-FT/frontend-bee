7f2de197cd7cbceb9405cf00bf1309675f50afba
Arthur05 <arthurfotso.bfkg@gmail.com>
2025-10-31 21:19:55 +0100

    feat: remove better-auth dependency and related auth client code

--- src/lib/auth/auth-client.ts
+++
@@ -1,7 +0,0 @@
-import { createAuthClient } from "better-auth/react";
-
-export const authClient = createAuthClient({
-  baseURL: "http://localhost:5000/api/auth",
-});
-
-export const { signIn, signUp, signOut, useSession } = authClient;
